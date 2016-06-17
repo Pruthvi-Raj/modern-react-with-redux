3ae32f9ee992c7cb0acb79a98bd6ea6dbfdfc6a9
Pruthvi Raj <pruthvi.n.v@gmail.com>
2016-06-17 17:07:10 +0530

    Initial commit

---
+++ youtube-app/src/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyChu3-K_zDy2lz-ezhK5BOEgwgiss61Jh0';
+
+// Create a new component. This component
+// should produce some HTML
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: [],selectedVideo: null};
+        this.videoSearch('Eminem');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term:term},(videos) => {
+            this.setState({videos, selectedVideo: videos[0]});
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}  />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+// Take this components generated HTML and put it
+// on the page
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
